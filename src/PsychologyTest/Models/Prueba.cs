c3522576fe46eae28bbf845b7d0cd848da7e2a84
J. Alejandro Cardona <alejandrocardona@outlook.com>
2016-11-03 21:33:33 -0500

    Modelo de pruebas psicologicas

---
+++ src/PsychologyTest/Models/Prueba.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PsychologyTest.Models
+{
+    public class PruebaPsicologica
+    {
+        public int Id { get; set; }
+        public string Descripcion { get; set; }
+        public List<Pregunta> Preguntas { get; set; }
+        public DateTime FechaCreado { get; set; }
+        public DateTime FechaModificado { get; set; }
+        public bool Activo { get; set; }
+    }
+
+    public class Pregunta
+    {
+        public int Id { get; set; }
+        public PruebaPsicologica Prueba { get; set; }
+        public TipoPregunta Tipo { get; set; }
+        public string Texto { get; set; }
+        public int Orden { get; set; }
+        public string RespuestaEsperada { get; set; }
+        public DateTime FechaModificado { get; set; }
+        public List<OpcionMultiple> Opciones { get; set; }
+    }
+
+    public class TipoPregunta
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public bool OpcionMultiple { get; set; }
+    }
+
+    public class OpcionMultiple
+    {
+        public int Id { get; set; }
+        public Pregunta PreguntaOrigen { get; set; }
+        public string Texto { get; set; }
+        public int Valor { get; set; }
+    }
+
+    public class Respuesta
+    {
+        public int Id { get; set; }
+        public PruebaAsignada Prueba { get; set; }
+        public Pregunta PreguntaOrigen { get; set; }
+        public DateTime FechaRespuesta { get; set; }
+        public string Valor { get; set; }
+    }
+
+    public class PruebaAsignada
+    {
+        public int Id { get; set; }
+        public Estudiante Encuestado { get; set; }
+        public DateTime FechaAsignacion { get; set; }
+        public DateTime FechaInicio { get; set; }
+        public DateTime FechaFinalizacion { get; set; }
+        public bool Iniciado { get; set; }
+        public bool Completado { get; set; }
+        public string Diagnostico { get; set; }
+    }
+}
--- src/PsychologyTest/Models/PsyTestContext.cs
+++ src/PsychologyTest/Models/PsyTestContext.cs
@@ -13,6 +13,12 @@ namespace PsychologyTest.Models
         public DbSet<DeletedUsers> UsuariosViejos { get; set; }
         public DbSet<Estudiante> Estudiantes { get; set; }
         public DbSet<Psicologo> Psicologos { get; set; }
+        public DbSet<PruebaPsicologica> PruebasPsicologicas{ get; set; }
+        public DbSet<Pregunta> Preguntas { get; set; }
+        public DbSet<TipoPregunta> TiposDePregunta{ get; set; }
+        public DbSet<OpcionMultiple> OpcionesMultiples{ get; set; }
+        public DbSet<PruebaAsignada> PruebasAsignadas{ get; set; }
+
 
         public PsyTestContext(IConfigurationRoot config, DbContextOptions options) 
             : base(options)
